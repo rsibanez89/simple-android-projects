056f4b105890790d35efce5489b1ba1e11592b5a
Rodrigo <rsibanez89@gmail.com>
2016-11-05 23:56:36 +1100

    simple menu using radio button. New running project generated by Android Studio.

---
+++ simple-menu-using-radio-button/app/src/main/java/rsibanez89/simple_menu_using_radio_button/MainActivity.java
@@ -0,0 +1,13 @@
+package rsibanez89.simple_menu_using_radio_button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
